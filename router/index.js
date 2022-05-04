f313b9f6bb689490eda58e055644844b0843d1af
Malthe <malthegram22@gmail.com>
2022-05-04 11:59:14 +0200

    fixing frotend... shit

--- router/index.js
+++ router/index.js
@@ -1,12 +1,10 @@
-import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
 
 import {
     renderText,
     setActiveLink,
-    adjustForMissingHash,
     loadTemplate,
     renderTemplate,
-} from "./Utils.js"
+} from "../Utils.js"
 
 window.addEventListener("load", async () => {
     const templateAbout = await loadTemplate('./Components/home/home.html')
