a891f488f6123a0631dd26560de5b1ab19ad7580
Simon Gredal <simon.gredal@icloud.com>
2022-05-16 13:39:34 +0200

    show showings on every weekday

--- Components/tickets/tickets.js
+++ Components/tickets/tickets.js
@@ -6,62 +6,48 @@
 
 import {handleHttpErrors} from "../../Utility/error.js";
 
-const SERVER = 'http://localhost:3000/showings'
+const SERVER = 'http://localhost:8080/api/movies'
 
 
 export async function data(match){
     const movieId = match?.params?.id
     console.log(movieId)
-    const weekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-    const saturday = document.getElementById("saturday")
-    const monday = document.getElementById("monday")
+    const weekdayColumns = {
+        "MONDAY": document.getElementById("monday"),
+        "TUESDAY": document.getElementById("tuesday"),
+        "WEDNESDAY": document.getElementById("wednesday"),
+        "THURSDAY": document.getElementById("thursday"),
+        "FRIDAY": document.getElementById("friday"),
+        "SATURDAY": document.getElementById("saturday"),
+        "SUNDAY": document.getElementById("sunday"),
+    }
 
     try {
-        const data = await fetch(SERVER)
+        const data = await fetch(`${SERVER}/${movieId}/showings`)
             .then(response => handleHttpErrors(response))
-            .then(data => {
-                console.log(data)
 
-                for (let i = 0; 0 < data.length; i++){
-                    if (data[i].startWeekday === "SATURDAY") {
-                        let startWeekDay = document.createElement("p")
-                        let startDate = document.createElement("p")
-                        let startTime = document.createElement("p")
-                        let anchor = document.createElement("a")
+        console.log(data)
 
-                        anchor.setAttribute("href", "https://www.google.com")
+        for (const showing of data) {
+            let startWeekDay = document.createElement("p")
+            let startDate = document.createElement("p")
+            let startTime = document.createElement("p")
+            let anchor = document.createElement("a")
 
-                        startWeekDay.innerText = data[i].startWeekday
-                        startDate.innerText = data[i].startDate
-                        startTime.innerText = data[i].startTime
+            anchor.setAttribute("href", "https://www.google.com")
 
-                        anchor.append(startWeekDay, startDate, startTime)
+            startWeekDay.innerText = showing.startWeekday
+            startDate.innerText = showing.startDate
+            startTime.innerText = showing.startTime
 
-                        saturday.appendChild(anchor)
-                    }
-                    if (data[i].startWeekday === "MONDAY"){
-                        let startWeekDay = document.createElement("p")
-                        let startDate = document.createElement("p")
-                        let startTime = document.createElement("p")
-                        let anchor = document.createElement("a")
+            anchor.appendChild(startWeekDay)
+            anchor.appendChild(startDate)
+            anchor.appendChild(startTime)
 
-                        anchor.setAttribute("href", "https://www.google.com")
+            weekdayColumns[ showing.startWeekday ].appendChild(anchor)
+        }
 
-                        startWeekDay.innerText = data[i].startWeekday
-                        startDate.innerText = data[i].startDate
-                        startTime.innerText = data[i].startTime
-
-                        anchor.append(startWeekDay, startDate, startTime)
-
-                        monday.appendChild(anchor)
-
-                    }
-
-                }
-
-            })
-
-    }catch (error){
+    } catch (error){
         console.error(error)
     }
 
