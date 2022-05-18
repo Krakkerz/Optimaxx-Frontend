d23c3e4149bcdb4533f945b425d6c55fb6a8399c
Malthe <malthegram22@gmail.com>
2022-05-18 12:33:29 +0200

    Seats WIP

--- Components/seats/seats.js
+++ Components/seats/seats.js
@@ -14,8 +14,15 @@ async function updateRoom(showingId) {
 }
 
 function renderSeats() {
+    document.querySelector(".theatre").innerHTML = ""
+
     const rowsInRoom = room.seats.map(seat => seat.row).reduce((acc, val) => Math.max(acc, val), Number.NEGATIVE_INFINITY)
     const seatsInRow = room.seats.map(seat => seat.number).reduce((acc, val) => Math.max(acc, val), Number.NEGATIVE_INFINITY)
+
+    const reserveButton = document.createElement("button")
+    reserveButton.classList.add("btn__reserve")
+    reserveButton.id = "btn__reserve"
+    reserveButton.innerText = "Reserve ticket!"
 
     const htmlSeatsLeft = document.createElement("div")
     htmlSeatsLeft.classList.add("cinema-seats")
@@ -58,62 +65,58 @@ function renderSeats() {
 
     document.querySelector(".theatre").appendChild(htmlSeatsLeft)
     document.querySelector(".theatre").appendChild(htmlSeatsRight)
+    document.querySelector(".theatre").appendChild(reserveButton)
 }
 
 
 export async function reserveSeat(match) {
-    const seats = document.querySelectorAll("div > .seat")
-    const movieId = match?.params.movieId
-    const showingId = match?.params.showing
-    const ticketAmount = document.getElementById("ticketsAmount")
-    const reserveButton = document.getElementById("btn__reserve")
+    const showingId = match?.params?.showing
     let seatsToBeReserved = []
     let options = makeOptions("POST", {
         accountId: 1,
-        seatsId: seatsToBeReserved,
+        seatIds: seatsToBeReserved,
         showingId: showingId,
     })
-
+    console.log(showingId)
     await updateRoom(showingId)
     renderSeats()
 
-    // .then(showing => {
-    //     console.log(showing)
-    //
-    //     for (let i = 0; i < seats.length; i++) {
-    //         seats.item(i).id = i.toString()
-    //         seats[i].addEventListener('click', function () {
-    //             if (seats[i].className === "seat") {
-    //                 seats[i].classList.add("active")
-    //                 seatsToBeReserved.push(i)
-    //             } else
-    //                 seats[i].classList.remove("active")
-    //         })
-    //         if (seats[i].className === "seat active") {
-    //             seatsToBeReserved.push(seats[i].id)
-    //
-    //         }
-    //     }
-    // })
+    const seats = document.querySelectorAll("div > .seat")
+    console.log(seats)
 
-    // }catch (error){
-    //     console.error(error)
-    // }
-    // Below code sets the color of the seats to white when pressed.
+        for (let i = 0; i < seats.length; i++) {
+            seats[i].addEventListener('click', function () {
+                console.log("Hello")
+                if (seats[i].className === ("seat unreserved")) {
+                    seats[i].classList.add("active")
+                    seatsToBeReserved.push(i)
+                } else
+                    seats[i].classList.remove("active")
+            })
+            if (seats[i].className === "seat active") {
+                seatsToBeReserved.push(seats[i].id)
 
-    // reserveButton.addEventListener('click', () => {
-    //     try {
-    //         fetch(`${SERVER}`, options)
-    //             .then(response => handleHttpErrors(response))
-    //         console.log(seatsToBeReserved)
-    //         for(const seat of seats){
-    //             seat.classList.remove("active")
-    //         }
-    //         alert(`Thank you for reserving ${seatsToBeReserved.length} ticket(s)`)
-    //     } catch (error) {
-    //         console.error(error)
-    //     }
-    // })
+            }
+        }
+    const reserveButton = document.getElementById("btn__reserve")
+
+    reserveButton.addEventListener('click', async () => {
+        try {
+            fetch(`${SERVER}`, options)
+                .then(response => handleHttpErrors(response))
+            console.log(seatsToBeReserved)
+            for (const seat of seats) {
+                seat.classList.remove("active")
+            }
+            alert(`Thank you reserving ${seatsToBeReserved.length} ticket(s)`)
+            await updateRoom(showingId)
+            renderSeats()
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+
 }
 
 
